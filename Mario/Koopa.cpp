3aa240298cd7c3c1869399a028d1cadadfa50ff4
usm <usm9401@gmail.com>
2024-02-26 13:09:19 +0900

    ㅇㅇ

--- Mario/Koopa.cpp
+++ Mario/Koopa.cpp
@@ -1,25 +1,25 @@
-#include "Koopa.h"
-
-#include <EngineCore/EngineResourcesManager.h>
-#include "MarioHelper.h"
-#include "PhysicsActor.h"
-
-Koopa::Koopa() 
-{
-}
-
-Koopa::~Koopa() 
-{
-}
-
-void Koopa::BeginPlay()
-{
-	SetName("Goomba");
-	NoDir = true;
-	Renderer = CreateImageRenderer(MarioRenderOrder::Monster);
-	Renderer->SetImage("Goomba.png");
-	Renderer->SetTransform({ {0,0 }, { 128,128 } });
-	AnimationAuto(Renderer, "Default", 0, 1);
-	SetAnimation("Default");
-}
-
+//#include "Koopa.h"
+//
+//#include <EngineCore/EngineResourcesManager.h>
+//#include "MarioHelper.h"
+//#include "PhysicsActor.h"
+//
+//Koopa::Koopa() 
+//{
+//}
+//
+//Koopa::~Koopa() 
+//{
+//}
+//
+//void Koopa::BeginPlay()
+//{
+//	SetName("Goomba");
+//	NoDir = true;
+//	Renderer = CreateImageRenderer(MarioRenderOrder::Monster);
+//	Renderer->SetImage("Goomba.png");
+//	Renderer->SetTransform({ {0,0 }, { 128,128 } });
+//	AnimationAuto(Renderer, "Default", 0, 1);
+//	SetAnimation("Default");
+//}
+//
--- Mario/Koopa.h
+++ Mario/Koopa.h
@@ -1,23 +1,23 @@
-#pragma once
-#include "Goomba.h"
-// 설명 :
-class Koopa : public Goomba
-{
-public:
-	// constrcuter destructer
-	Koopa();
-	~Koopa();
-
-	// delete Function
-	Koopa(const Koopa& _Other) = delete;
-	Koopa(Koopa&& _Other) noexcept = delete;
-	Koopa& operator=(const Koopa& _Other) = delete;
-	Koopa& operator=(Koopa&& _Other) noexcept = delete;
-
-protected:
-	void BeginPlay() override;
-	void Tick(float _DeltaTime) override;
-private:
-
-};
-
+//#pragma once
+//#include "Goomba.h"
+//// 설명 :
+//class Koopa : public Goomba
+//{
+//public:
+//	// constrcuter destructer
+//	Koopa();
+//	~Koopa();
+//
+//	// delete Function
+//	Koopa(const Koopa& _Other) = delete;
+//	Koopa(Koopa&& _Other) noexcept = delete;
+//	Koopa& operator=(const Koopa& _Other) = delete;
+//	Koopa& operator=(Koopa&& _Other) noexcept = delete;
+//
+//protected:
+//	void BeginPlay() override;
+//	void Tick(float _DeltaTime) override;
+//private:
+//
+//};
+//
